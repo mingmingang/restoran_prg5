2ef0aa1dc4e20b476347134d3d56c5b7bf9b3844
mingminghar <saharromansah@gmail.com>
2024-10-23 15:35:37 +0700

    git push -u origin main

--- application/views/restaurant.php
+++ application/views/restaurant.php
@@ -1085,12 +1085,12 @@
     <!-- ===============================================-->
     <!--    JavaScripts-->
     <!-- ===============================================-->
-    <script src="vendors/@popperjs/popper.min.js"></script>
-    <script src="vendors/bootstrap/bootstrap.min.js"></script>
-    <script src="vendors/is/is.min.js"></script>
+    <script src="<?php echo base_url('restaurant/vendors/@popperjs/popper.min.js')?>"></script>
+    <script src="<?php echo base_url('restaurant/vendors/bootstrap/bootstrap.min.js')?>"></script>
+    <script src="<?php echo base_url('restaurant/vendors/is/is.min.js')?>"></script>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
-    <script src="vendors/fontawesome/all.min.js"></script>
-    <script src="assets/js/theme.js"></script>
+    <script src="<?php echo base_url('restaurant/vendors/fontawesome/all.min.js')?>"></script>
+    <script src="<?php echo base_url('restaurant/assets/js/theme.js')?>"></script>
 
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;900&amp;display=swap" rel="stylesheet">
   </body>
